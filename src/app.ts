99665631d459b94c0ba51bf941b4f8a6a062b56e
Tuấn Anh <anhnht@youmed.vn>
2022-02-17 07:31:01 +0700

    Add some middleware

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,29 @@
+import { Logger } from './core/utils';
 import { Route } from '@core/interfaces';
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import morgan from 'morgan';
 
 class App {
   public app: express.Application;
   public port: string | number;
-
+  public production: boolean;
   constructor(routes: Route[]) {
     this.app = express();
     this.port = process.env.PORT || 5000;
+    this.production = process.env.NODE_ENV == 'production' ? true : false;
 
     this.initializeRoutes(routes);
     this.connectToDatabase();
+    this.initializeMiddleware();
   }
 
   public listen() {
     this.app.listen(this.port, () => {
-      console.log(`Server is listening on port ${this.port}`);
+      Logger.info(`Server is listening on port ${this.port}`);
     });
   }
 
@@ -26,17 +33,29 @@ class App {
     });
   }
 
+  private initializeMiddleware() {
+    if (this.production) {
+      this.app.use(hpp());
+      this.app.use(helmet());
+      this.app.use(morgan('combined'));
+      this.app.use(cors({ origin: 'your.domain.com', credentials: true }));
+    } else {
+      this.app.use(morgan('dev'));
+      this.app.use(cors({ origin: true, credentials: true }));
+    }
+  }
+
   private connectToDatabase() {
     try {
       const connectString = process.env.MONGODB_URI;
       if (!connectString) {
-        console.log('Connection string is invalid');
+        Logger.error('Connection string is invalid');
         return;
       }
         mongoose.connect(connectString);
-      console.log('Database connected...');
+        Logger.info('Database connected...');
     } catch (error) {
-      console.log('Connect to database error');
+      Logger.error('Connect to database error');
     }
   }
 }
--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,5 +1,5 @@
-// import Logger from './logger';
+import Logger from './logger';
 // import { isEmptyObject } from './helpers';
 // import validateEnv from './validate_env';
 
-// export { Logger, validateEnv, isEmptyObject };
+export { Logger};
