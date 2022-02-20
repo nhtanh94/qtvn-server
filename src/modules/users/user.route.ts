89a67b85738ddc18916b656018833f0d35aa18fd
Tuấn Anh <anhnht@youmed.vn>
2022-02-20 16:59:47 +0700

    add API delete User

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -3,6 +3,7 @@ import { Route } from '@core/interfaces';
 import { Router } from 'express';
 import UsersController from './users.controller';
 import validationMiddleware from '@core/middleware/validation.middleware';
+import { authMiddleware } from '@core/middleware';
 
 export default class UsersRoute implements Route {
   public path = '/api/users';
@@ -14,12 +15,14 @@ export default class UsersRoute implements Route {
     this.initializeRoutes();
   }
 
+
   private initializeRoutes() {
     this.router.post(
       this.path,
       validationMiddleware(RegisterDto, true),
       this.usersController.register
-    ); //POST: http://localhost:5000/api/users
+    );
+
     this.router.put(
       this.path + '/:id',
       validationMiddleware(RegisterDto, true),
@@ -27,5 +30,18 @@ export default class UsersRoute implements Route {
     );
 
     this.router.get(this.path + '/:id', this.usersController.getUserById);
+
+    this.router.get(this.path, this.usersController.getAll);
+
+    this.router.get(
+      this.path + '/paging/:page',
+      this.usersController.getAllPaging
+    );
+
+    this.router.delete(
+      this.path + '/:id',
+      authMiddleware,
+      this.usersController.deleteUser
+    );
   }
 }
